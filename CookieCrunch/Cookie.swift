5c75f27159be2f164c1891a501f2a4745e752203
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-03 09:34:37 +1000

    Add Cookie file (represents model state)

---
+++ CookieCrunch/Cookie.swift
@@ -0,0 +1,43 @@
+//
+//  Cookie.swift
+//  CookieCrunch
+//
+//  Created by Naveed Siddiqui on 2/07/2015.
+//  Copyright (c) 2015 10eighteen. All rights reserved.
+//
+
+import SpriteKit
+
+enum CookieType: Int {
+    case Unknown = 0, Croissant, Cupcake, Danish, Donut, Macaroon, SugarCookie
+    
+    var spriteName: String {
+        let spriteNames = [
+            "Croissant",
+            "Cupcake",
+            "Danish",
+            "Donut",
+            "Macaroon",
+            "SugarCookie"]
+        
+        return spriteNames[rawValue - 1]
+    }
+    
+    var highlightedSpriteName: String {
+        return spriteName + "-Highlighted"
+    }
+
+}
+
+class Cookie {
+    var column: Int
+    var row: Int
+    let cookieType: CookieType
+    var sprite: SKSpriteNode?
+    
+    init(column: Int, row: Int, cookieType: CookieType) {
+        self.column = column
+        self.row = row
+        self.cookieType = cookieType
+    }
+}
