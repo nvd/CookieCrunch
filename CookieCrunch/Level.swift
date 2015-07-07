f5d466b19e4714058c3d0358402c133cc7c89346
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-07 14:27:49 +1000

    Add Level

---
+++ CookieCrunch/Level.swift
@@ -0,0 +1,16 @@
+//
+//  Level.swift
+//  CookieCrunch
+//
+//  Created by Naveed Siddiqui on 7/07/2015.
+//  Copyright (c) 2015 10eighteen. All rights reserved.
+//
+
+import Foundation
+
+let NumColumns = 9
+let NumRows = 9
+
+class Level {
+    private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+}
