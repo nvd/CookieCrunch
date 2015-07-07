3e9b61528075342235174a8f27a3f9b0761069e7
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-07 16:03:34 +1000

    Add sprites to cookies layer

--- CookieCrunch/GameScene.swift
+++ CookieCrunch/GameScene.swift
@@ -30,4 +30,19 @@ class GameScene: SKScene {
         cookiesLayer.position = layerPosition
         gameLayer.addChild(cookiesLayer)
     }
+
+    func addSpritesForCookies(cookies: Set<Cookie>) {
+        for cookie in cookies {
+            let sprite = SKSpriteNode(imageNamed: cookie.cookieType.spriteName)
+            sprite.position = pointForColumn(cookie.column, row:cookie.row)
+            cookiesLayer.addChild(sprite)
+            cookie.sprite = sprite
+        }
+    }
+
+    func pointForColumn(column: Int, row: Int) -> CGPoint {
+        return CGPoint(
+            x: CGFloat(column)*TileWidth + TileWidth/2,
+            y: CGFloat(row)*TileHeight + TileHeight/2)
+    }
 }
