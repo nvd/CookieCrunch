a7d1fad6acb5681ad7c2af0a2b6581ed02cc158e
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-07 14:40:19 +1000

    Add method in Level to query cookie at position

--- CookieCrunch/Level.swift
+++ CookieCrunch/Level.swift
@@ -5,4 +5,10 @@ let NumRows = 9
 
 class Level {
     private var cookies = Array2D<Cookie>(columns: NumColumns, rows: NumRows)
+    
+    func cookieAtColumn(column: Int, row: Int) -> Cookie? {
+        assert(column >= 0 && column < NumColumns)
+        assert(row >= 0 && row < NumRows)
+        return cookies[column, row]
+    }
 }
