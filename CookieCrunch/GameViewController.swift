1e02d43028f648a947aab3a79920e24a35262570
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-07 16:09:52 +1000

    Add cookies to scene

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -3,31 +3,41 @@ import SpriteKit
 
 class GameViewController: UIViewController {
     var scene: GameScene!
-    
+    var level: Level!
+
     override func prefersStatusBarHidden() -> Bool {
         return true
     }
-    
+
     override func shouldAutorotate() -> Bool {
         return true
     }
-    
+
     override func supportedInterfaceOrientations() -> Int {
         return Int(UIInterfaceOrientationMask.AllButUpsideDown.rawValue)
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         // Configure the view.
         let skView = view as! SKView
         skView.multipleTouchEnabled = false
-        
+
         // Create and configure the scene.
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
-        
+
         // Present the scene.
         skView.presentScene(scene)
+    }
+
+    func beginGame() {
+        shuffle()
+    }
+
+    func shuffle() {
+        let newCookies = level.shuffle()
+        scene.addSpritesForCookies(newCookies)
     }
 }
