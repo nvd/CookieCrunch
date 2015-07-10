80e6782c21c5d5ff65eac20ed18ae2d0ceb66563
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-10 18:17:09 +1000

    Add Level.init to load from json file

--- CookieCrunch/Level.swift
+++ CookieCrunch/Level.swift
@@ -43,4 +43,24 @@ class Level {
         assert(row >= 0 && row < NumRows)
         return tiles[column, row]
     }
+    
+    init(filename: String) {
+        // 1
+        if let dictionary = Dictionary<String, AnyObject>.loadJSONFromBundle(filename) {
+            // 2
+            if let tilesArray: AnyObject = dictionary["tiles"] {
+                // 3
+                for (row, rowArray) in enumerate(tilesArray as! [[Int]]) {
+                    // 4
+                    let tileRow = NumRows - row - 1
+                    // 5
+                    for (column, value) in enumerate(rowArray) {
+                        if value == 1 {
+                            tiles[column, tileRow] = Tile()
+                        }
+                    }
+                }
+            }
+        }
+    }
 }
