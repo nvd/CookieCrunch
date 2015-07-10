4d56dfa99ff8195263d66bbb28c6f5ecfd6fc0a0
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-10 18:36:28 +1000

    Add tileslayer for tile sprites

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -28,8 +28,9 @@ class GameViewController: UIViewController {
         scene = GameScene(size: skView.bounds.size)
         scene.scaleMode = .AspectFill
 
-        level = Level(filename: "Level_1")
+        level = Level(filename: "Levels/Level_1")
         scene.level = level
+        scene.addTiles()
 
         // Present the scene.
         skView.presentScene(scene)
