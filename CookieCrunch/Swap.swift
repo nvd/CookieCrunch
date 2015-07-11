e7daa7a95e7128a7f6c015fcede8e8d9ff8cee3f
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-11 11:21:36 +1000

    Add Swap

---
+++ CookieCrunch/Swap.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+struct Swap: Printable {
+    let cookieA: Cookie
+    let cookieB: Cookie
+    
+    init(cookieA: Cookie, cookieB: Cookie) {
+        self.cookieA = cookieA
+        self.cookieB = cookieB
+    }
+    
+    var description: String {
+        return "swap \(cookieA) with \(cookieB)"
+    }
+}
