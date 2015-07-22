3a9e22b90d940c0408269b0f5ac7c4daa45980e9
Naveed Siddiqui <nsiddiqui@fairfaxmedia.com.au>
2015-07-22 20:40:44 +1000

    Update score

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -79,6 +79,10 @@ class GameViewController: UIViewController {
         }
 
         scene.animateMatchedCookies(chains) {
+            for chain in chains {
+                self.score += chain.score
+            }
+            self.updateLabels()
             let columns = self.level.fillHoles()
             self.scene.animateFallingCookies(columns) {
                 let columns = self.level.topUpCookies()
